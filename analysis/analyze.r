94b59cb8d6a8c8ffec6a7fc7bb2ff3ae3817ac9c
Max Siegel <maxs@mit.edu>
2024-01-29 18:43:05 -0500

    analysis updates

--- analysis/analyze.r
+++ analysis/analyze.r
@@ -1,16 +1,28 @@
 library(tidyverse)
 
-
+setwd("/Users/maxs/explanation_experiment/analysis")
 ddir <- normalizePath("../data")
 sub_files <- dir(ddir, full.names = TRUE)
 
-keep_cols <- c("task", "correct", "id", "rt", "target_side")
+keep_cols <- c("response", "id", "rt")
 
-data <- sub_files %>%
+all_data <- sub_files %>%
   map(read_csv) %>%
-  reduce(rbind) %>%
-  filter(task == "response")  %>%
-  select(keep_cols)
+  reduce(rbind)
+## select(any_of(keep_cols))
 
-data %>%
-  group_by(id)
+data <- all_data %>%
+  filter(trial_type == "html-slider-response")
+
+trials <- data %>%
+  group_by(trial_index) %>%
+  filter(condition == 1) %>%
+  mutate(response = as.numeric(response)) %>%
+  summarize(average = mean(response))
+
+ggplot(trials, aes(x = factor(trial_index), y = average)) +
+  geom_col() +
+  scale_x_discrete(breaks = trials$trial_index, labels = trials$trial_index) +
+  xlab("trial number")
+
+  ## scale_x_discrete(breaks = trials$trial_index, labels = trials$trial_index)
